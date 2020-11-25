15439b29a604836e4e0297b831b6dfd931f1da64
Pedro Alves <pedroalvesk@protonmail.com>
2020-11-25 02:09:01 +0000

    Added api.js and removed some files

---
+++ front/source/src/store/actions/api.js
@@ -0,0 +1,3 @@
+export const API_LOGIN = "http://127.0.0.1:8081/auth/login"
+export const API_GET_PLACES = "http://127.0.0.1:8081/front/users/places"
+export const API_SOCKETIO = "http://127.0.0.1:8081/"
